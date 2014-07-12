3296d020205397a3d2f2eb31beea5e1e4ac648c1
emerson-j <jackemerson@jacks-mbp.home>
2014-07-12 23:39:42 +0100

    Added more comments

--- app/Larabook/Registration/Events/UserRegistered.php
+++ app/Larabook/Registration/Events/UserRegistered.php
@@ -4,8 +4,15 @@ use Larabook\Users\User;
 
 class UserRegistered {
 
+    /**
+     * @var \Larabook\Users\User
+     */
     public $user;
 
+
+    /**
+     * @param User $user
+     */
     function __construct(User $user)
     {
         $this->user = $user;

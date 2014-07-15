f1823b970e451b3b6d9e274942c9ecd5527b0eb8
emerson-j <jackemerson@jacks-mbp.home>
2014-07-15 20:49:39 +0100

    Tests are now green.

--- tests/functional/SignUpCept.php
+++ tests/functional/SignUpCept.php
@@ -6,7 +6,7 @@ $I->am('a guest');
 $I->wantTo('Sign up for a Larabook account');
 
 $I->amOnPage('/');
-$I->click('Register');
+$I->click('Sign Up');
 $I->seeCurrentUrlEquals('/register');
 
 $I->fillField('username', 'JohnDoe');
